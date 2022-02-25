cfe800421708cecf1f8a704bb57c00f464010474
zhuhangan <807279070@qq.com>
2022-02-25 11:06:24 +0800

    Initial commit

---
+++ cloudreve/run.sh
@@ -0,0 +1,12 @@
+docker run -d \
+  --name cloudreve \
+  -e PUID=0 \
+  -e PGID=0 \
+  -e TZ="Asia/Shanghai" \
+  -p 5212:5212 \
+  --restart=unless-stopped \
+  -v $PWD/cloudreve/uploads:/cloudreve/uploads \
+  -v $PWD/cloudreve/config:/cloudreve/config \
+  -v $PWD/cloudreve/db:/cloudreve/db \
+  -v $PWD/cloudreve/avatar:/cloudreve/avatar \
+  xavierniu/cloudreve
