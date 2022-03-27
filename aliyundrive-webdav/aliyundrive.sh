34672f3211e646b57aaeb7ec6c488687811ab3f7
zhuhangan <807279070@qq.com>
2022-03-27 23:57:54 +0800

    f

--- aliyundrive-webdav/aliyundrive.sh
+++ aliyundrive-webdav/aliyundrive.sh
@@ -1,6 +1,6 @@
 docker run -d --name=aliyundrive-webdav --restart=unless-stopped -p 8100:8080 \
   -v  $PWD/aliyundrive-webdav/data/:/etc/aliyundrive-webdav/ \
   -e REFRESH_TOKEN='b172c1e0cc4942388d81ccc8f4867624' \
-  -e WEBDAV_AUTH_USER=admin \
-  -e WEBDAV_AUTH_PASSWORD=admin \
+  -e WEBDAV_AUTH_USER=zhuhangan \
+  -e WEBDAV_AUTH_PASSWORD=zhu666456.. \
   messense/aliyundrive-webdav
