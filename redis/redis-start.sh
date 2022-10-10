4c031f2b9037477dee75ddf28494739e9d5b602e
zhuhangan <zhuhangan@qiyi.com>
2022-10-10 17:58:34 +0800

    redis

--- redis/redis-start.sh
+++ redis/redis-start.sh
@@ -1,2 +1,2 @@
  sudo docker rm myredis
-sudo docker run -v /conf:/usr/local/etc/redis --name myredis amd64/redis redis-server /usr/local/etc/redis/redis.conf
+sudo docker run -v ./conf:/usr/local/etc/redis --name myredis amd64/redis redis-server /usr/local/etc/redis/redis.conf
