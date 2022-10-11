ff6f200a3cc850dbd762079518e4553e7d52745e
zhuhangan <zhuhangan@qiyi.com>
2022-10-11 18:18:23 +0800

    redis

--- redis/redis-start.sh
+++ redis/redis-start.sh
@@ -1,2 +1,2 @@
  sudo docker rm myredis
-sudo docker run -d -v -p  $PWD/conf:/usr/local/etc/redis --restart=on-failure --name myredis amd64/redis redis-server /usr/local/etc/redis/redis.conf
+sudo docker run -d -p 6379:6379 -v "$PWD"/conf:/usr/local/etc/redis --restart=on-failure --name myredis amd64/redis redis-server /usr/local/etc/redis/redis.conf
