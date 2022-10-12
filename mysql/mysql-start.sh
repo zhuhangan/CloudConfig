5048160ba50d00000571d721e71d1f6849114aa9
zhuhangan <zhuhangan@qiyi.com>
2022-10-12 10:23:40 +0800

    redis

---
+++ mysql/mysql-start.sh
@@ -0,0 +1,10 @@
+# shellcheck disable=SC1009
+docker run --name some-mysql -v $PWD/data:/var/lib/mysql \
+ --restart=always \
+ --network my-net \
+ -e MYSQL_ROOT_PASSWORD=zhu666456 \
+ -d mysql:8.0.31 \
+ -p 3306:3306 \
+ --character-set-server=utf8mb4 \
+ --collation-server=utf8mb4_unicode_ci \
+ --lower_case_table_names=1
